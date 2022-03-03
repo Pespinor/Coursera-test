2b9f954e52175a8221c16b33bca8dba00a3aaa1a
Pespinor <97239290+Pespinor@users.noreply.github.com>
2022-03-03 00:37:12 -0300

    Update script.js

--- mod5_sol/js/script.js
+++ mod5_sol/js/script.js
@@ -81,14 +81,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // *** start ***
 // On first load, show home view
 showLoading("#main-content");
-$ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitely setting the flag to get JSON from server processed into an object literal
+$ajaxUtils.sendGetRequest(allCategoriesUrl,buildAndShowHomeHTML,true); 
 });
-// *** finish **
 
-
+// ***** <---- TODO: STEP 1: Substitute [...] ******
+// Explicitely setting the flag to get JSON from server processed into an object literal
+  // *** finish ** 
+  
+  
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
